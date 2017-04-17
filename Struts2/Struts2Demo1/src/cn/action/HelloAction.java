5cf7e28102938d8bee17a22362b57c45def69bae
yangzhe <385609722@qq.com>
2017-04-17 18:27:02 +0800

    另外一个工程

---
+++ Struts2/Struts2Demo1/src/cn/action/HelloAction.java
@@ -0,0 +1,12 @@
+package cn.action;
+
+/**
+ * Created by yangzhe on 17/4/17.
+ */
+public class HelloAction {
+
+    public String execute(){
+        System.out.println("execute");
+        return "ok";
+    }
+}
